7d87d6be299e62ab90ec24159a3a641fecb033c0
Illia16 <illia2143@gmail.com>
2021-09-30 23:19:55 -0400

    Svelte added

--- index.js
+++ index.js
@@ -1,34 +1,43 @@
-module.exports = {
-    init: function(e) {
-        var xlsx = require('xlsx');
-        var file = xlsx.readFile('./text.xlsx');
-        var data = [];
-        var sheets = file.SheetNames;
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
 
-        for (let i = 0; i < sheets.length; i++) {
-            var getData = xlsx.utils.sheet_to_json(file.Sheets[file.SheetNames[i]]);
-            getData.forEach((res) => {
-                data.push(res)
-            });
-        }
+app.on("ready", () => {
+  const mainWindow = new BrowserWindow();
+  mainWindow.loadFile(path.join(__dirname, "public/index.html"));
+  mainWindow.webContents.openDevTools();
 
-        console.log('e', e);
-        console.log('e', process);
-        // document.querySelector('#excell-file').addEventListener('change', this.onFileInput, false);
+});
 
+// module.exports = {
+//     init: function(e) {
+//         var xlsx = require('xlsx');
+//         var file = xlsx.readFile('./text.xlsx');
+//         var data = [];
+//         var sheets = file.SheetNames;
 
-        console.log('data', data)
-    },
-    onFileInput: function(e) {
-        var files = e.target.files, f = files[0];
-        console.log('files', files);
-        var reader = new FileReader();
+//         for (let i = 0; i < sheets.length; i++) {
+//             var getData = xlsx.utils.sheet_to_json(file.Sheets[file.SheetNames[i]]);
+//             getData.forEach((res) => {
+//                 data.push(res)
+//             });
+//         }
 
-        reader.onload = function(e) {
-            var data = new Uint8Array(e.target.result);
-            var workbook = XLSX.read(data, {type: 'array'});
-            console.log('workbook', workbook);
-        };
-        reader.readAsArrayBuffer(f);
-    },
-}
+//         console.log('e', e);
+//         console.log('e', process);
+//         // document.querySelector('#excell-file').addEventListener('change', this.onFileInput, false);
+
+//         console.log('data', data)
+//     },
+//     onFileInput: function(e) {
+//         var files = e.target.files, f = files[0];
+//         console.log('files', files);
+//         var reader = new FileReader();
+
+//         reader.onload = function(e) {
+//             var data = new Uint8Array(e.target.result);
+//             var workbook = XLSX.read(data, {type: 'array'});
+//             console.log('workbook', workbook);
+//         };
+//         reader.readAsArrayBuffer(f);
+//     },
+// }
